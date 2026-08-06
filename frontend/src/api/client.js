3b756af20999907ba271685427c558a1e0219669
Nil Savaliya <nilsavaliya123@gmail.com>
2026-08-06 15:30:15 +0530

    change the baseURL

--- frontend/src/api/client.js
+++ frontend/src/api/client.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://criclive-enterprise-api.onrender.com',
+  baseURL: 'https://criclive-enterprise-api.onrender.com/api/v1',
   headers: {
     'Content-Type': 'application/json'
   }
